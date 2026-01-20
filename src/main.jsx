4d8f2e43022a674af9efa93f4854012406a25062
tamimMuntakimm <130441317+tamimMuntakim@users.noreply.github.com>
2026-01-20 23:16:16 +0600

    Router.jsx renamed

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import router from './Routes/router.jsx'
+import router from './Routes/Router.jsx'
 import { RouterProvider } from 'react-router'
 import AuthProvider from './Providers/AuthProvider.jsx'
 
